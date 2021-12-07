0ea720c42aa770078784c93e7a8e0c0e782387eb
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-07 14:51:45 +0000

    Move user data from seed to data folder

---
+++ db/data/development-data/users.js
@@ -0,0 +1,20 @@
+exports.seedUser = [
+    {
+        email: "example@email.com",
+        username: "josephCode",
+        avatar_url: "https://maxcdn.icons8.com/Share/icon/Cinema/avatar1600.png",
+        about_me: "I love to sing",
+        first_name: "Jonathan",
+        last_name: "Ramon",
+        phone_number: 07780862354,
+        social: {
+            youtube: "https://www.youtube.com/channel/UCWUGSN8Bb_c2QwPUfYmHwNA",
+            facebook: "https://www.facebook.com/jonathan.joseph.357/",
+            instagram: "https://www.instagram.com/jonathanjoseph76/"
+        },
+        voice: ["Alto"],
+        Groups: ["The altos"],
+    }
+];
+
+//moonglade, genie, cakevealbladerunner, groundhogdaylyrics, beeteaglepieapplepie
