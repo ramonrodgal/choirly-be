3855ae1f3110d933650cb79c3f180022b7dbaf65
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-07 14:52:58 +0000

    Seed choir data into Atlas

--- db/seeds/run-seed.js
+++ db/seeds/run-seed.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const User = require('../../schemas/users');
+const Choir = require('../../schemas/choir');
 require('dotenv').config();
-const {seedUser} = require('./seed');
+
+const {seedUser} = require('../data/development-data/users');
+const {seedChoir} = require('../data/development-data/choir')
 
 mongoose.connect(process.env.DATABASE_URL).then(() =>{
     console.log('connected to db')
@@ -12,10 +15,11 @@ mongoose.connect(process.env.DATABASE_URL).then(() =>{
 const seedDB = async () => {
     await User.deleteMany({});
     await User.insertMany(seedUser)
+
+    await Choir.deleteMany({});
+    await Choir.insertMany(seedChoir)
 }
 
 seedDB().then(()=>{
     mongoose.connection.close();
 })
-
-console.log(seedUser);
