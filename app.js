ce3a524f8efb74f6ac1e4c813f3da968d3108371
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-07 12:40:43 +0000

    Update express server to use json in body requests

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 require('dotenv').config();
 
-mongoose.connect(process.env.DATABASE_URL).then(() =>{
-  console.log('connected to db')
-}).catch(err => {
-  console.log(err);
-});
+app.use(express.json());
 
 const app = express();
 const port = 9000;
 
-
+mongoose.connect(process.env.DATABASE_URL).then(() =>{
+  console.log('connected to db')
+}).catch(err => { 
+  console.log(err);
+});
 
 app.listen(port, () => {
   console.log(`Listening in port ${port}`);
