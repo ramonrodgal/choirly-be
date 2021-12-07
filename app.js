edfff4596c155e2fabd96185f67d7508159e8ec7
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-07 11:55:15 +0000

    Connect server with Atlas DB

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+mongoose.connect(process.env.DATABASE_URL).then(() =>{
+  console.log('connected to db')
+}).catch(err => {
+  console.log(err);
+});
 
 const app = express();
 const port = 9000;
+
+
 
 app.listen(port, () => {
   console.log(`Listening in port ${port}`);
