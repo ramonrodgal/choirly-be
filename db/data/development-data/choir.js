ee426ae326251bf602af19455f345d2ec6d6c9c6
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-07 14:52:15 +0000

    Create choir data

---
+++ db/data/development-data/choir.js
@@ -0,0 +1,33 @@
+exports.seedChoir = [{
+    name: "African Children's Choir",
+    location: "Manchester",
+    description: `The African Children's Choir is made up of some of the neediest and most vulnerable children in their countries. Many have lost one or both parents to poverty or disease. The African Children's Choir helps these children break away from the everyday cycle of poverty and hopelessness. Before being selected to join the Choir, children, generally aged between 7 and 11 attend Music for Life camps. These camps are fun and stimulating environments that provide a break from the daily hardships the young children face at home.
+    
+    From the hundreds of children who attend the camps, approximately 50 will be chosen to audition for the Choir. Games, crafts, music and devotions are some of the activities the children look forward to at camp. Once the children are selected, Choir Teams will spend a few days visiting the children's homes to determine their needs and suitability for tour life. They then have the difficult task of selecting the group of children who will form the next African Children's Choir.`,
+    avatar_url: "https://www.singers.com/group/images/AfricanChildrensChoir.jpg",
+    social: {
+        website: "https://www.singers.com/group/African-Childrens-Choir/",
+        facebook: "https://www.facebook.com/africanchildrenschoir/",
+    },
+    leader: "Choir master",
+    members: ["moonglade", "genie", "cakevealbladerunner", "groundhogdaylyrics", "beeteaglepieapplepie"],
+    events: [],
+    files: [
+        {
+            filename: "song.mp3",
+            type:"song",
+            path:"http://google.com"
+        },
+        {
+            filename: "songsheet.docx",
+            type:"document",
+            path:"http://google.com",
+        },
+        {
+            filename: "picture.jpeg",
+            type:"image",
+            path:"http://google.com"
+        },
+    ],
+    messages: [],
+}];
