688f9a896b3f7f4e1fb3ca7c3a55ee13542cd7c1
josephcoder76 <chenstyle@talk21.com>
2021-12-08 12:01:44 +0000

    Create choirs data

--- db/data/development-data/choirs.js
+++ db/data/development-data/choirs.js
@@ -13,13 +13,13 @@ exports.seedChoir = [
     },
     leader: "cakevealbladerunner",
     members: [
-      "moonglade",
+      "josephCode",
       "genie",
+      "moonglade",
       "cakevealbladerunner",
-      "groundhogdaylyrics",
-      "beeteaglepieapplepie",
+      "korus76",
     ],
-    events: [],
+    events: [], //Populate the events from the events data once this has been created
     files: [
       {
         filename: "song.mp3",
@@ -33,6 +33,88 @@ exports.seedChoir = [
       },
       {
         filename: "picture.jpeg",
+        type: "image",
+        path: "http://google.com",
+      },
+    ],
+    messages: [],
+  },
+  {
+    name: "Liverpool 64",
+    location: "Liverpool",
+    description: `Liverpool64 was formed in 2018. It has grown quickly and as of May 2021 has over 100 members registered to sing across its soprano, alto, tenor and bass sections. The choir rehearses weekly on Monday evenings 6.30-8pm in the cathedral.`,
+    avatar_url:
+      "https://logovtor.com/wp-content/uploads/2021/04/liverpool-cathedral-logo-vector.png",
+    social: {
+      website:
+        "https://www.liverpoolcathedral.org.uk/cathedral-music/our-choirs/liverpool-64/",
+      facebook: "https://www.facebook.com/LiverpoolCathedral/",
+      //  twitter:
+      //  "https://twitter.com/LivCathedral?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor",
+    },
+    leader: "korus76",
+    members: [
+      "josephCode",
+      "genie",
+      "moonglade",
+      "cakevealbladerunner",
+      "korus76",
+    ],
+    events: [], //Populate the events from the events data once this has been created
+    files: [
+      {
+        filename: "majestic symphony1.mp3",
+        type: "song",
+        path: "http://google.com",
+      },
+      {
+        filename: "piano music sheets symphony1.pdf",
+        type: "document",
+        path: "http://google.com",
+      },
+      {
+        filename: "rehearsal13thFeb2022.jpeg",
+        type: "image",
+        path: "http://google.com",
+      },
+    ],
+    messages: [],
+  },
+  {
+    name: "Chester Bach Singers",
+    location: "Chester",
+    description: `Chester Bach Singers, conducted by Martin Bussey,  is one of the finest chamber choirs in the North West of England. With its associated baroque Orchestra, Chester Bach Singers presents a season of concerts in and around Chester and has appeared in concert series across the UK. Established in September 1976, the 2016/17 season marked our 40th Anniversary Season.
+
+    2020 has been a strange year for the choir; coronavirus took its toll on our 2019/20 and 2020/21 seasons, and has meant a complete overhaul of our forward planning. We are pleased to be able to offer our usual Christmas cheer on Saturday 18th December, and another of our famous Come And Sing workshops – Handel’s Coronation Anthems – on Saturday 5th February 2022. Book now to avoid missing out!`,
+    avatar_url:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b4/Chester_choir.jpg",
+    social: {
+      website: "https://www.chesterbachsingers.org.uk/",
+      facebook: "https://www.facebook.com/ChesterBachSingers/",
+      //instagram: "https://www.instagram.com/thebachchoir/?hl=en",
+    },
+    leader: "genie",
+    members: [
+      "josephCode",
+      "genie",
+      "moonglade",
+      "cakevealbladerunner",
+      "korus76",
+    ],
+    events: [], //Populate the events from the events data once this has been created
+    files: [
+      {
+        filename: "Bach.mp3",
+        type: "song",
+        path: "http://google.com",
+      },
+      {
+        filename: "choirtimetable.pdf",
+        type: "document",
+        path: "http://google.com",
+      },
+      {
+        filename: "Christmascarols.jpeg",
         type: "image",
         path: "http://google.com",
       },
