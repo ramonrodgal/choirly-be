fd26aa420350be825c9edb01153e52793a9328cc
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-08 13:24:25 +0000

    Create GET /api/user/:username endpoint

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,9 +1,19 @@
-const { fetchUsers } = require("../models/users.models");
+const { fetchUsers, fetchUserByUsername } = require("../models/users.models");
 
 exports.getUsers = async (req, res, next) => {
   try {
     const users = await fetchUsers();
     res.status(200).send({ users });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUserByUsername = async (req, res, next) => {
+  const { username } = req.params;
+  try {
+    const user = await fetchUserByUsername(username);
+    res.status(200).send({ user });
   } catch (err) {
     next(err);
   }
--- models/users.models.js
+++ models/users.models.js
@@ -3,3 +3,8 @@ const User = require("../schemas/user");
 exports.fetchUsers = async () => {
   return await User.find();
 };
+
+exports.fetchUserByUsername = async (username) => {
+  const user = await User.find({ username: username });
+  return user[0];
+};
