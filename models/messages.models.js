0721c154c6da98e43df86deebd867df6904dc947
ramonrodgal <ramonrodriguez@gmail.com>
2021-12-09 19:24:15 +0000

    Update postMessage to handle invalid body

--- models/messages.models.js
+++ models/messages.models.js
@@ -8,6 +8,30 @@ exports.fetchMessagesByChoirId = async (choir_id) => {
 };
 
 exports.insertMessageByChoirId = async (body) => {
+  const requiredFields = ["choir", "author", "title", "body"];
+  let allFields = true;
+  let allFieldTypes = true;
+
+  const fieldTypesReference = {
+    choir: "string",
+    author: "string",
+    title: "string",
+    body: "string",
+  };
+
+  for (let requiredField of requiredFields) {
+    if (!body.hasOwnProperty(requiredField)) {
+      allFields = false;
+    }
+    if (fieldTypesReference[requiredField] !== typeof body[requiredField]) {
+      allFieldTypes = false;
+    }
+  }
+
+  if (!allFields || !allFieldTypes) {
+    return Promise.reject({ status: 400, msg: "Bad Request. Invalid Body" });
+  }
+
   const message = new GroupMessage(body);
   return await message.save();
 };
