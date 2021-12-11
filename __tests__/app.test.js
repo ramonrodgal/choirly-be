7313f2fdf5b382aa7be4ddfe894afa7691674502
ramonrodgal <ramonrodriguezgalvez@gmail.com>
2021-12-11 22:28:37 +0000

    Test /api/choirs/:choir_id/users/:user_id to respond with a message for invalid request

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -502,6 +502,42 @@ describe("/api/choirs/:choir_id/users/:username", () => {
 
       expect(choir.members.includes(username)).toBe(true);
     });
+    test.only("status:404 responds with a message for user not found", async () => {
+      const choir_id = "61b0c4c065064fdfb889a148";
+      const username = "notAUser";
+
+      const {
+        body: { msg },
+      } = await request(app)
+        .patch(`/api/choirs/${choir_id}/users/${username}`)
+        .expect(404);
+
+      expect(msg).toBe("User not found");
+    });
+    test.only("status:404 responds with a message for choir not found", async () => {
+      const choir_id = "61b0c4c065064fdfb889a248";
+      const username = "genie";
+
+      const {
+        body: { msg },
+      } = await request(app)
+        .patch(`/api/choirs/${choir_id}/users/${username}`)
+        .expect(404);
+
+      expect(msg).toBe("Choir not found");
+    });
+    test.only("status:400 responds with a message for invalid choir id", async () => {
+      const choir_id = "invalid";
+      const username = "genie";
+
+      const {
+        body: { msg },
+      } = await request(app)
+        .patch(`/api/choirs/${choir_id}/users/${username}`)
+        .expect(400);
+
+      expect(msg).toBe("Bad request. Invalid choir id");
+    });
   });
 });
 
